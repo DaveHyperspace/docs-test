81c3718b17924495410762b9ba2226deb85c22f1
DaveHyperspace <dave@hyperspace.us>
2023-05-16 00:49:10 +0100

    init demo

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Hyperspace</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/DaveHyperspace/docs-test',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/DaveHyperspace/docs-test',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Footer text here',
   },
-}
+};
 
-export default config
+export default config;
